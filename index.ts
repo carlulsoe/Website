e7788d4f5f15f3fa4b3d60262c32193d425aaf24
carlulsoe <34673973+carlulsoe@users.noreply.github.com>
2022-01-21 13:55:40 +0100

    pushed

---
+++ index.ts
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const port = 8000;
+
+//app.use(express.static('/css'));
+app.use('/css', express.static(__dirname + '/css'))
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html', {root: __dirname});
+  })
+
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`);
+}); 
---
+++ readme.md
@@ -0,0 +1,5 @@
+run: npx tailwindcss -i ./css/index.css -o ./css/output.css --watch
+This recompiles tailwindcss
+
+run: node index.ts
+This runs the nodejs server
